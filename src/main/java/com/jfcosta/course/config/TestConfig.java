201a86f345fc1bbce8a7c876c71e2eba53f20358
Francisco Jonas <jonas.filho26@gmail.com>
2021-02-23 22:52:33 -0300

    Payment, one-to-one association

--- src/main/java/com/jfcosta/course/config/TestConfig.java
+++ src/main/java/com/jfcosta/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.jfcosta.course.entities.Category;
 import com.jfcosta.course.entities.Order;
 import com.jfcosta.course.entities.OrderItem;
+import com.jfcosta.course.entities.Payment;
 import com.jfcosta.course.entities.Product;
 import com.jfcosta.course.entities.User;
 import com.jfcosta.course.enums.OrderStatus;
@@ -89,6 +90,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 
 	
